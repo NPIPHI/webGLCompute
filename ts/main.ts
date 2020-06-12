52b3848c3129558984164bc2464a8c0d25f7b8c6
NPIPHI <nwp2@illinois.edu>
2020-06-12 17:47:21 -0500

    basic html

--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,12 @@
-console.log("wa");
+var canvas : HTMLCanvasElement;
+var gl : WebGL2RenderingContext;
+var ctx : CanvasRenderingContext2D;
+
+function init(){
+    canvas = document.getElementById("canv") as HTMLCanvasElement;
+    gl = canvas.getContext("webgl2");
+    gl.clearColor(0.5, 0, 0.5, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+}
+
+init();
